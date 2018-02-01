f70d9d1efeed3596ad6f2a3faa4e34a638a30f96
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-02-01 15:37:56 -0600

    fixin' stuff

--- packages/demos/angular/src/app/virtual-scroll/virtual-scroll.module.ts
+++ packages/demos/angular/src/app/virtual-scroll/virtual-scroll.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IonicAngularModule } from '@ionic/angular';
 import { VirtualScrollPageComponent } from './virtual-scroll-page.component';
 import { VirtualScrollRoutingModule } from './virtual-scroll-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    IonicAngularModule,
     VirtualScrollRoutingModule
   ],
   declarations: [VirtualScrollPageComponent],
